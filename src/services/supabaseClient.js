a92cc353b27d482411fd4091d4357b9d1c3b8da9
ianjj2 <ianmarcos.efata2@gmail.com>
2025-06-04 14:56:20 -0300

    fix: corrigir variáveis de ambiente para REACT_APP_SUPABASE_URL e REACT_APP_SUPABASE_ANON_KEY

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://xxpvouzikaaouyxrhuzc.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4cHZvdXppa2Fhb3V5eHJodXpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNTUzNzgsImV4cCI6MjA2NDYzMTM3OH0.OlvLCCMRboHrDoA422ztqhSr70RO3zxd3CzQ5KM50QI';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://xxpvouzikaaouyxrhuzc.supabase.co';
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4cHZvdXppa2Fhb3V5eHJodXpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNTUzNzgsImV4cCI6MjA2NDYzMTM3OH0.OlvLCCMRboHrDoA422ztqhSr70RO3zxd3CzQ5KM50QI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
